b393800e326df5cf8588113acca2adeedc00f105
iradhakakumanusk <iradhakakumanu@gmail.com>
2026-09-11 17:04:07 +0530

    Convert deliver.bat from bash to Windows batch script

--- jenkins/scripts/deliver.bat
+++ jenkins/scripts/deliver.bat
@@ -1,26 +1,22 @@
-#!/usr/bin/env bash
+@echo off
 
-echo 'The following Maven command installs your Maven-built Java application'
-echo 'into the local Maven repository, which will ultimately be stored in'
-echo 'Jenkins''s local Maven repository (and the "maven-repository" Docker data'
-echo 'volume).'
-set -x
+echo The following Maven command installs your Maven-built Java application
+echo into the local Maven repository.
+
 mvn jar:jar install:install help:evaluate -Dexpression=project.name
-set +x
 
-echo 'The following command extracts the value of the <name/> element'
-echo 'within <project/> of your Java/Maven project''s "pom.xml" file.'
-set -x
-NAME=`mvn -q -DforceStdout help:evaluate -Dexpression=project.name`
-set +x
+echo.
+echo Extracting project name...
+for /f "delims=" %%A in ('mvn -q -DforceStdout help:evaluate -Dexpression=project.name') do set "NAME=%%A"
 
-echo 'The following command behaves similarly to the previous one but'
-echo 'extracts the value of the <version/> element within <project/> instead.'
-set -x
-VERSION=`mvn -q -DforceStdout help:evaluate -Dexpression=project.version`
-set +x
+echo Project Name: %NAME%
 
-echo 'The following command runs and outputs the execution of your Java'
-echo 'application (which Jenkins built using Maven) to the Jenkins UI.'
-set -x
-java -jar target/${NAME}-${VERSION}.jar
+echo.
+echo Extracting project version...
+for /f "delims=" %%A in ('mvn -q -DforceStdout help:evaluate -Dexpression=project.version') do set "VERSION=%%A"
+
+echo Project Version: %VERSION%
+
+echo.
+echo Running Java application...
+java -jar "target\%NAME%-%VERSION%.jar"
